a3e80eb31780c0ffc0579129108804d90b3bf75e
brayanmb07 <52015419+brayanmb07@users.noreply.github.com>
2019-06-19 22:21:22 -0400

    text

---
+++ For Fun/login.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+
+class write{
+    private: 
+        
+    public:
+    
+       
+};
+
+
+int main()
+{   
+   cout<<"*****************************************"<< endl;
+   cout<<"**************     login    *************"<< endl;
+   cout<<"*****************************************"<< endl;
+
+}
